6514504d40ef2dc24aa6b5901377ab046091a880
Rahanur Mister Rahan <46438258+rjmister24@users.noreply.github.com>
2019-01-10 17:02:04 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,34 @@
+printf "\n\n"
+printf "1. Install FbInfo"
+printf "\n"
+cd $HOME
+cd Facebook_clone
+git clone http://github.com/rjmister24/FbInfo
+cd FbInfo
+ls
+printf "Install complate..."
+printf "\n"
+
+
+printf "\n\n"
+printf "2. Install FbBrute"
+printf "\n"
+cd $HOME
+cd Facebook_clone
+git clone http://github.com/rjmister24/FbBrute
+cd FbBrute
+ls
+printf "Install complate..."
+printf "\n"
+
+
+printf "\n\n"
+printf "3. Install Logcracks"
+printf "\n"
+cd $HOME
+cd Facebook_clone
+git clone http://github.com/rjmister24/Logcracks
+cd Logcracks
+ls
+printf "Install complate..."
+printf "\n"
